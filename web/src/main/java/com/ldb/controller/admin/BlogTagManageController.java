c528416b22119db6a71182481b3c17be4dfa3a8b
llldddbbb <571002217@qq.com>
2017-04-30 16:18:40 +0800

    写博客页面添加标签

--- web/src/main/java/com/ldb/controller/admin/BlogTagManageController.java
+++ web/src/main/java/com/ldb/controller/admin/BlogTagManageController.java
@@ -74,4 +74,5 @@ public class BlogTagManageController {
             return null;
         }
     }
+
 }
--- web/src/main/resources/static/background/js/main.js
+++ web/src/main/resources/static/background/js/main.js
@@ -1,10 +1,4 @@
-﻿/*
-
-@Name：不落阁后台模板源码 
-@Author：Absolutely 
-@Site：http://www.lyblogs.cn
-
-*/
+﻿
 var element;
 var $;
 layui.define(['element', 'layer', 'util', 'pagesize', 'form'], function (exports) {
